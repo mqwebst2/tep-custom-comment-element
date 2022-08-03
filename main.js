39615eccc776b0257aefcc58d91e1eb3f024337d
mqwebst2 <marques.q.webster@gmail.com>
2022-08-03 11:49:29 -0500

    updates to commenting system

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class CommentComponent extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<form>
+    this.innerHTML = `<form id="form" name="form">
       <label for="name">Name: </label>
       <input type="text" id="name" name="name" required />
       <label for="email">Email: </label>
@@ -9,9 +9,32 @@ class CommentComponent extends HTMLElement {
       <textarea id="comment" name="comment" cols="44" rows="10" required></textarea><br /><br />
       <input type="checkbox" id="checkbox" name="checkbox" />
       <label for="checkbox">Checking this box agrees to submit/post this comment.</label><br /><br />
-      <button type="submit" id="submit-btn">Submit</button>
+      <input type="submit" id="submit-btn" value="Submit" />
     </form>`;
   }
 }
 
 customElements.define("comment-component", CommentComponent);
+
+class CommentResponse extends HTMLElement {
+  connectedCallback() {}
+}
+
+customElements.define("comment-response", CommentResponse);
+
+const form = document.getElementById("form");
+const output = document.getElementById("output");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let formData = new FormData(form);
+
+  let input = [];
+
+  for (let value of formData.values()) {
+    input.push(value);
+  }
+
+  output.innerHTML = `${input}`;
+});
