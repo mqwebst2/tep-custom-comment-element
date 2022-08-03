a5b51bdfd001d41928ab6fd3fc96a086d934a5ad
mqwebst2 <marques.q.webster@gmail.com>
2022-08-02 23:32:46 -0500

    Changes to the tep-commenting-system

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ class CommentComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<form>
       <label for="name">Name: </label>
-      <input type="text" id="name" name="name" />
+      <input type="text" id="name" name="name" required />
       <label for="email">Email: </label>
-      <input type="text" id="email" name="email" /><br /><br />
+      <input type="text" id="email" name="email" required /><br /><br />
       <label for="comment">Comment: </label>
-      <textarea id="comment" name="comment" cols="44" rows="10"></textarea>
+      <textarea id="comment" name="comment" cols="44" rows="10" required></textarea><br /><br />
+      <input type="checkbox" id="checkbox" name="checkbox" />
+      <label for="checkbox">Checking this box agrees to submit/post this comment.</label><br /><br />
+      <button type="submit" id="submit-btn">Submit</button>
     </form>`;
   }
 }
